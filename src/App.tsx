4dcc8e234417fa11ccd50439890269fb021d1d38
RTX <alrababah.saedemad@gmail.com>
2024-10-16 14:21:57 +0300

    sign up page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignInPage from "./screen/signin_Page/index";
+import SignIn from "./screen/signIn/index";
+import SignUp from "./screen/signUp";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignInPage />} />
-        <Route path="/about" element={""} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
---
+++ src/screen/signUp/index.tsx
@@ -0,0 +1,74 @@
+import ButtonComponent from "../../component/button";
+import IntroImage from "../../component/introImg";
+import TextComponent from "../../component/textComponent";
+
+function SignUp() {
+  return (
+    <div className="flex items-center justify-center h-[80vh] ">
+      <div className="flex-col  rounded-md md:p-16 w-[90%] md:w-[50%]">
+        <TextComponent text="Sign Up" className=" text-2xl font-bold" />
+        <div className="flex space-x-2 py-4">
+          <TextComponent
+            text={`Already have an account?`}
+            className="text-text_gray"
+          />
+          <a href="/" className="text-text_blue font-bold">
+            Sign in
+          </a>
+        </div>
+        <form className=" pt-6">
+          <div className="flex gap-4">
+            <div className="flex-col w-[50%]">
+              <TextComponent text="First Name" className="py-3" />
+              <input
+                type="text"
+                className=" border border-gray w-full p-2 h-10 rounded-md transition-all"
+              />
+            </div>
+            <div className="flex-col w-[50%]">
+              <TextComponent text="Last Name" className="py-3" />
+              <input
+                type="text"
+                className=" border border-gray w-full p-2 h-10 rounded-md transition-all"
+              />
+            </div>
+          </div>
+          <div className="Date of birth">
+            <TextComponent text="Last Name" className="py-3" />
+            <input
+              type="date"
+              className=" border border-gray w-full p-2 h-10 rounded-md transition-all"
+            />
+          </div>
+          <div className="flex-col">
+            <TextComponent text="Email address" className="py-3" />
+            <input
+              type="email"
+              className=" border border-gray w-full p-2 h-10 rounded-md transition-all"
+            />
+          </div>
+          <div className="flex-col">
+            <TextComponent text="Password" className="pb-3 pt-6" />
+            <input
+              type="password"
+              className=" border border-gray w-full p-2 h-10 rounded-md"
+            />
+          </div>
+          <div className="flex-col">
+            <TextComponent text="Confirm Password" className="pb-3 pt-6" />
+            <input
+              type="password"
+              className=" border border-gray w-full p-2 h-10 rounded-md"
+            />
+          </div>
+
+          <div className="py-6"></div>
+          <ButtonComponent text="Sign Up" />
+        </form>
+      </div>
+      <IntroImage />
+    </div>
+  );
+}
+
+export default SignUp;
