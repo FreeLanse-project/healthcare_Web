89c4d55ae06edd6287be991eef579fbce8b22b41
RTX <alrababah.saedemad@gmail.com>
2024-10-16 14:21:39 +0300

    add image componenet

---
+++ src/component/introImg.tsx
@@ -0,0 +1,13 @@
+const IntroImage = () => {
+  return (
+    <div className=" max-md:hidden w-[50%] overflow-hidden">
+      <img
+        src="src\assets\introImage.jpg"
+        alt=""
+        className="h-[80vh]  object-fill"
+      />
+    </div>
+  );
+};
+
+export default IntroImage;
