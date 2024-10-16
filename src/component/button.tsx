99d2fb9c649487945706b7a6854ef13d7fd68812
RTX <alrababah.saedemad@gmail.com>
2024-10-16 14:53:41 +0300

    add classname as prop

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -3,13 +3,18 @@ import React from "react";
 type ButtonComponentProps = {
   text: string;
   onclick?: () => void;
+  className?: string;
 };
 
-const ButtonComponent: React.FC<ButtonComponentProps> = ({ text, onclick }) => {
+const ButtonComponent: React.FC<ButtonComponentProps> = ({
+  text,
+  onclick,
+  className,
+}) => {
   return (
     <button
       type="submit"
-      className=" w-full bg-primary font-medium text-secondary hover:opacity-80 transition-opacity py-2 rounded-md"
+      className={`w-full bg-primary font-medium text-secondary hover:opacity-80 transition-opacity py-2 rounded-md ${className}`}
       onClick={onclick}
     >
       {text}
